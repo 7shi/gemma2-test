26bd1888e4c471df86fe6f7cdc3af2bb13453d84
7shi <7shi@live.jp>
2024-08-08 22:23:45 +0900

    read text from file

--- inferno-2.py
+++ inferno-2.py
@@ -1,43 +1,7 @@
 import common
 
-inferno1 = [line[4:] for line in """
-  1 Nel mezzo del cammin di nostra vita
-  2 mi ritrovai per una selva oscura,
-  3 ché la diritta via era smarrita.
-  4 Ahi quanto a dir qual era è cosa dura
-  5 esta selva selvaggia e aspra e forte
-  6 che nel pensier rinova la paura!
-  7 Tant’ è amara che poco è più morte;
-  8 ma per trattar del ben ch’i’ vi trovai,
-  9 dirò de l’altre cose ch’i’ v’ho scorte.
- 10 Io non so ben ridir com’ i’ v’intrai,
- 11 tant’ era pien di sonno a quel punto
- 12 che la verace via abbandonai.
- 13 Ma poi ch’i’ fui al piè d’un colle giunto,
- 14 là dove terminava quella valle
- 15 che m’avea di paura il cor compunto,
- 16 guardai in alto e vidi le sue spalle
- 17 vestite già de’ raggi del pianeta
- 18 che mena dritto altrui per ogne calle.
- 19 Allor fu la paura un poco queta,
- 20 che nel lago del cor m’era durata
- 21 la notte ch’i’ passai con tanta pieta.
- 22 E come quei che con lena affannata,
- 23 uscito fuor del pelago a la riva,
- 24 si volge a l’acqua perigliosa e guata,
- 25 così l’animo mio, ch’ancor fuggiva,
- 26 si volse a retro a rimirar lo passo
- 27 che non lasciò già mai persona viva.
- 28 Poi ch’èi posato un poco il corpo lasso,
- 29 ripresi via per la piaggia diserta,
- 30 sì che ’l piè fermo sempre era ’l più basso.
- 31 Ed ecco, quasi al cominciar de l’erta,
- 32 una lonza leggera e presta molto,
- 33 che di pel macolato era coverta;
- 34 e non mi si partia dinanzi al volto,
- 35 anzi ’mpediva tanto il mio cammino,
- 36 ch’i’ fui per ritornar più volte vòlto.
-"""[1:-1].split("\n")]
+with open("inferno-2.txt", "r") as f:
+    inferno1 = f.read().strip().split("\n")
 
 prompt = "Translate into English with a breakdown:\n"
 
@@ -86,8 +50,7 @@ seeds = iter([
     165123746, 83518692, 151223161, 197674250,
 ])
 
-for lnum in range(1):
-#for lnum in range(0, len(inferno1), 3):
+for lnum in range(0, len(inferno1), 3):
     print()
     print("#", lnum + 1)
     print()
