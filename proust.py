0db66c4b768bb3b6ae3826dbaa78409652e80e72
7shi <7shi@live.jp>
2024-08-10 03:47:51 +0900

    verifying proust using various quantizations

--- proust.py
+++ proust.py
@@ -3,10 +3,26 @@ import common
 with open("proust.txt", "r") as f:
     text = f.read().strip()
 messages = [{ "role": "user", "content": "Translate into English:\n" + text }]
-seeds = iter([144350804, 627871955, 197720406, 553534933])
+seed = 144350804
 
-for model in common.models:
+models = """
+gemma2:2b-instruct-q4_0
+gemma2:2b-instruct-q4_K_S
+gemma2:2b-instruct-q4_K_M
+gemma2:2b-instruct-q8_0
+gemma2:9b-instruct-q4_0
+gemma2:9b-instruct-q4_K_S
+gemma2:9b-instruct-q4_K_M
+llama3.1:8b-instruct-q4_0
+llama3.1:8b-instruct-q4_K_S
+llama3.1:8b-instruct-q4_K_M
+mistral-nemo:12b-instruct-2407-q4_0
+mistral-nemo:12b-instruct-2407-q4_K_S
+mistral-nemo:12b-instruct-2407-q4_K_M
+"""[1:-1].split("\n")
+
+for model in models:
     print()
     print("#", model)
     print()
-    common.query(model, messages, seed=next(seeds))
+    common.query(model, messages, seed=seed)
